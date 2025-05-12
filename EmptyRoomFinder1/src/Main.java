d7d369448c9e833f6e851db14b3cf9a49960f88c
hyckju <hyckjoo0125@gmail.com>
2025-05-12 23:21:43 +0900

    25.05.12

--- EmptyRoomFinder1/src/Main.java
+++ EmptyRoomFinder1/src/Main.java
@@ -3,4 +3,18 @@ import java.util.List;
 import java.util.Scanner;
 
 ///콘솔 기반 빈 강의실 찾기 프로그램
-public class EmptyRoomFinder {}
+public class EmptyRoomFinder {
+
+    /// TimeSiot: 예약된 시간 구간 표현
+    public static class TimeSlot {
+        public int startHour;
+        public int startMinute;
+        public int endHour;
+        public int endMinute;
+
+        public TimeSlot(int startHour, int startMinute, int endHour, int endMinute) {
+            this.startHour = startHour;
+            this.startMinute = startMinute;
+        }
+    }
+}
