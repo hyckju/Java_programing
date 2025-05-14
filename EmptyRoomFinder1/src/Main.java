34eb79bb9c6ff4340f47b7641c5df828b3269ccb
hyckju <hyckjoo0125@gmail.com>
2025-05-14 23:10:08 +0900

    25.05.14.2

--- EmptyRoomFinder1/src/Main.java
+++ EmptyRoomFinder1/src/Main.java
@@ -17,11 +17,19 @@ public class EmptyRoomFinder {
         }
 
         public boolean contains(int hour) {
+
             return hour >= startHour && hour <= endHour;
         }
 
         public String toString() {
+
             return String.format("[%2d:00 ~ %2d:00]", startHour, endHour);
+        }
+
+        /// Classroom: 강의실 이름과 예약된 시간 목록 불러오기
+        static class Classroom {
+            private final String name;
+            private final List<TimeSlot> timeSlots = new ArrayList<>();
         }
 
     }
