a8061fc25632ca2435565d29bcf338d4a4a607d2
hyckju <hyckjoo0125@gmail.com>
2025-05-28 23:06:25 +0900

    25.05.28

--- EmptyRoomFinder1/src/EmptyRoomFinder.java
+++ EmptyRoomFinder1/src/EmptyRoomFinder.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 ///콘솔 기반 빈 강의실 찾기 프로그램
 public class EmptyRoomFinder {
 
-    /// TimeSiot: 예약된 시간 구간 표현
+    /// TimeSlot: 예약된 시간 구간 표현
     static class TimeSlot {
         private final int startHour;
         private final int endHour;
@@ -52,19 +52,55 @@ public class EmptyRoomFinder {
             }
             /// 예약된 전체 스케줄 문자열로 반환
             public String scheduleString() {
-                if (occuousTimeSlots().isEmpty()) {
+                if (occupiedTimeSlots().isEmpty()) {
                     return "예정된 강의 없음";
                 }
                 StringBuilder sb = new StringBuilder();
-                for (TimeSlot timeSlot : occuousTimeSlots()) {
-                    sb.append(timeSlot).oppend(" ");
+                for (TimeSlot timeSlot : occupiedTimeSlots()) {
+                    sb.append(timeSlot).append(" ");
                 }
                 return sb.toString().trim();
+            }
+
+            private List<TimeSlot> occupiedTimeSlots() {
+                return timeSlots;
             }
 
             public String getRoomName() {
                 return name;
             }
+        }
+
+        public static void main(String[] args) {
+            List<Classroom> classrooms = new ArrayList<>();
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("조회할 시간을 입력하세요 (0 ~ 23): ");
+            int hour;
+            try {
+                hour = scanner.nextInt();
+                if (hour < 0 || hour > 23) {
+                    throw new IllegalArgumentException("올바른 시간을 입력하세요.");
+                }
+            }catch (NumberFormatException e){
+                System.out.println("올바른 시간을 입력하세요.");
+                scanner.close();
+                return;
+            }
+
+            System.out.printf("\n== %02d:00 기준 빈 강의실 목록 ==\n ", hour);
+            boolean any = false;
+            for (Classroom classroom : classrooms) {
+                if (classroom.isEmptyAt(hour)) {
+                    System.out.printf("%s\n", classroom.getRoomName());
+                    any = true;
+                }
+            }
+            if (!any) {
+                System.out.println("빈 강의실이 없습니다.");
+            }
+
+            scanner.close();
 
         }
 
